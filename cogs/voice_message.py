39839aab7aaeb5df3e593d6e28c7cb07f20b4404
Aleksei Dugin <alexadush@yandex.ru>
2021-12-29 12:25:16 +0500

    Debug voice message

--- cogs/voice_message.py
+++ cogs/voice_message.py
@@ -16,6 +16,7 @@ class VoiceMessage(commands.Cog):
         t1 = gtts.gTTS(text, lang=message_language)
         file_name = str(time() * 10000) + ".mp3"
         t1.save(file_name)
+        await ctx.send(text, "Language", message_language)
         await ctx.send(
             f"Аудиосообщение от {ctx.message.author.name}",
             file=discord.File(file_name)
