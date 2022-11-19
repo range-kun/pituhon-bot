c0b49b2a6979ef47a3ee873a856ca6ec189d0c28
range <alexadush@yandex.ru>
2022-11-19 14:17:43 +0300

    Change way to obtain configuration

--- configuration.py
+++ configuration.py
@@ -1,27 +1,28 @@
-import os
-import yaml
+from decouple import config
 
-DB_NAME = "d1d79slc0v9cp5"
-DB_HOST = "ec2-18-200-55-205.eu-west-1.compute.amazonaws.com"
 MAX_HIST_RETRIEVE_RECORDS = 10
 DEFAULT_TRANSLATE_LANGUAGE = "russian"
 PYTHON_BOT_ID = 698973448772386927
 TEST_CHANNEL_ID = 698975367326728352
 MAIN_CHANNEL_ID = 873248515042738176
 VOTE_TIME = 60  # minutes
-REDIS_HOST = "redis-15587.c300.eu-central-1-1.ec2.cloud.redislabs.com"
-REDIS_PORT = 15587
 UMBRA_ID = 439474652079718410
 
-try:
-    with open('config.yaml', 'r') as my_file:
-        secret_configs = yaml.safe_load(my_file)
-except FileNotFoundError:
-    secret_configs = os.environ
-    
-TOKEN = secret_configs.get('BOT_TOKEN')
-API_KEY = secret_configs.get('API_KEY')
-SEARCH_ENGINE_ID = secret_configs.get('SEARCH_ENGINE_ID')
-DB_USER = secret_configs.get('DB_USER')
-DB_PASSWORD = secret_configs.get('DB_PASSWORD')
-REDIS_PASSWORD = secret_configs.get('REDIS_PASSWORD')
+# Discord token
+TOKEN = config('BOT_TOKEN')
+
+# Google
+API_KEY = config('API_KEY')
+SEARCH_ENGINE_ID = config('SEARCH_ENGINE_ID')
+
+# DB
+DB_USER = config('DB_USER')
+DB_PASSWORD = config('DB_PASSWORD')
+DB_NAME = config('DB_NAME')
+DB_HOST = "ec2-18-200-55-205.eu-west-1.compute.amazonaws.com"
+
+
+# Redis
+REDIS_PASSWORD = config('REDIS_PASSWORD')
+REDIS_HOST = config('REDIS_HOST')
+REDIS_PORT = config('REDIS_PORT')
